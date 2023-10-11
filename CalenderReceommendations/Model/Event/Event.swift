a6f2f6ee4fe7892774fe2e89c214f92bd44226e8
Sriteja Chilakamarri <sriteja.csriteja@gmail.com>
2023-10-11 22:14:18 +0530

    Initial commit with Calendar Kit

---
+++ CalenderReceommendations/Model/Event/Event.swift
@@ -0,0 +1,8 @@
+//
+//  Event.swift
+//  CalenderReceommendations
+//
+//  Created by Sriteja Chilakamarri on 10/10/2023.
+//
+
+import Foundation
---
+++ CalenderReceommendations/Utils/LocationHandler.swift
@@ -0,0 +1,28 @@
+//
+//  LocationHandler.swift
+//  CalenderReceommendations
+//
+//  Created by Sriteja Chilakamarri on 10/10/2023.
+//
+
+import CoreLocation
+
+class LocationHandler: NSObject, CLLocationManagerDelegate {
+    
+    static let shared = LocationHandler()
+    var locationManager: CLLocationManager!
+    var location: CLLocation?
+    
+    override init() {
+        super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+}
