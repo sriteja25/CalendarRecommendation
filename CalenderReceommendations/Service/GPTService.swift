f803f7da03049283ac4963c36eb607df6223a616
Sriteja Chilakamarri <sriteja.csriteja@gmail.com>
2023-10-13 14:14:10 +0530

    gitignore added

--- CalenderReceommendations/Service/GPTService.swift
+++ CalenderReceommendations/Service/GPTService.swift
@@ -16,7 +16,7 @@ class GPTService {
         
         let prompt = "I am attending an event named \(event.title) at \(locationName). It starts at \(event.startDate) and ends at \(event.endDate). The weather is likely to be \(startWeather.temp) degrees centigrade at the start of event with \(startWeather.weather[0].description) and \(endWeather.temp) degrees centigrade at the end of event with \(endWeather.weather[0].description). 1. Give me a very very specific clothing recommendation (Shirt, pant, short, frock, saree, cotton, wool, nylon etc) for weather 2. if an umbrella or jacket are necessary in 5 words. 3. Anything based on event \(event.title) 4.small random fact involving \(event.title) and event city. Give the output properly numbered without side headings ready to display on UILabel."
         print(prompt)
-        let apiKey = "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"
+        let apiKey = API_KEYS.GPT_KEY
         let api = ChatGPTAPI(apiKey: apiKey)
         Task {
             do {
--- CalenderReceommendations/Service/WeatherService.swift
+++ CalenderReceommendations/Service/WeatherService.swift
@@ -11,7 +11,7 @@ import Foundation
 
 class WeatherService {
     private let baseURL = "https://api.openweathermap.org/data/2.5/onecall" // Weather map API end point for hourly forecast
-    private let apiKey = "10d83272107b3579bbec3790805f50f1" // API key for the weathermap
+    private let apiKey = API_KEYS.WEATHER_KEY // API key for the weathermap
     private let units = "metric"
     
     func fetchWeatherData(latitude: String, longitude: String, completion: @escaping (Result<WeatherResponse, Error>) -> Void) {
